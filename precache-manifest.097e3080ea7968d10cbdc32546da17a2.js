d7c66b681f0afeae6b951901931b60914a27b10f
silenostass <56112964+silenostass@users.noreply.github.com>
2020-07-02 19:51:36 +0300

    Updates

---
+++ precache-manifest.097e3080ea7968d10cbdc32546da17a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa744f01f4efaa8b65b5b5fe3b2cca3",
+    "url": "/gen-but/index.html"
+  },
+  {
+    "revision": "8d5077afbad9291d5d83",
+    "url": "/gen-but/static/css/main.306bee7f.chunk.css"
+  },
+  {
+    "revision": "c3693de062505df994bf",
+    "url": "/gen-but/static/js/2.5f4dba22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gen-but/static/js/2.5f4dba22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5077afbad9291d5d83",
+    "url": "/gen-but/static/js/main.d593159e.chunk.js"
+  },
+  {
+    "revision": "00c6dc0e33bd5d9e4086",
+    "url": "/gen-but/static/js/runtime-main.cc87410c.js"
+  }
+]);
